642925a0b2b06e5a4f3840213394bded9b70e240
Fatima Asif <fatima01asiflive@gmail.com>
2026-07-12 15:13:01 +0500

    User Controller

---
+++ backend/multer.js
@@ -0,0 +1,13 @@
+const multer=require("multer");
+
+const storage=multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/') // Specify the directory where files will be stored
+    },
+    filename: function (req, file, cb) {
+        const uniqueSufix=Date.now() + '-' + Math.round(Math.random() * 1e9);
+        const filename=file.originalname.split(".")[0]
+        cb(null, filename + '-' + uniqueSufix +".png") // Specify the filename for uploaded files
+    },
+});
+exports.upload=multer({ storage: storage });
